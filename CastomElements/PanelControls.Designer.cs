c4af9ad506ab9c343ef58dc9fdb0ed7b1fbf6073
Admin <Admin@DESKTOP-MAHVHFA>
2025-12-08 13:01:45 +0300

    PanelControls

---
+++ CastomElements/PanelControls.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ЛР_8.CastomElements
+{
+    partial class PanelControls
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(575, 400);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // PanelControls
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "PanelControls";
+            this.Size = new System.Drawing.Size(575, 400);
+            this.Load += new System.EventHandler(this.PanelControls_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ CastomElements/PanelControls.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ЛР_8.DataModel;
+
+namespace ЛР_8.CastomElements
+{
+    public partial class PanelControls : UserControl
+    {
+        public PanelControls()
+        {
+            InitializeComponent();
+        }
+
+        private void PanelControls_Load(object sender, EventArgs e)
+        {
+            Model1 model1 = new Model1();
+            model1.Product.ToList().ForEach(x => 
+            flowLayoutPanel1.Controls.Add(new ProductControl(x)));
+        }
+    }
+}
