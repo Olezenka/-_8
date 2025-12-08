2cf7ed2f1321fd2cf21285a46a7df8eb01b0574c
Admin <Admin@DESKTOP-MAHVHFA>
2025-12-08 12:58:47 +0300

    прописан код для ProductControl

--- CastomElements/ProductControl.cs
+++ CastomElements/ProductControl.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ЛР_8.DataModel;
 
 namespace ЛР_8.CastomElements
 {
     public partial class ProductControl : UserControl
     {
-        public ProductControl()
+        public ProductControl(Product product)
         {
             InitializeComponent();
+            Fill(product);
         }
-
+        private void Fill(Product product)
+        {
+            labelName.Text = product.Title;
+            labelMinCost.Text = product.MinCostForAgent.ToString();
+            MemoryStream ms = new MemoryStream(product.Image, 0, product.Image.Length);
+            pictureBox1.Image = Image.FromStream(ms);
+        }
         private void ProductControl_Load(object sender, EventArgs e)
         {
 
